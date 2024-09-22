ba36c795b5394b81ea97397ddc464ff323c64552
Vitor Fontenele <142616878+Vitorfol@users.noreply.github.com>
2024-09-22 20:18:08 +0000

    home section

--- scripts/section-selector.js
+++ scripts/section-selector.js
@@ -5,6 +5,16 @@ document.addEventListener('DOMContentLoaded', () => {
     navButtons.forEach(button => {
         button.addEventListener('click', (event) => {
             const targetId = event.target.getAttribute('data-target');
+
+            if (targetId === '#home') {
+                event.preventDefault();
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                });
+                return;
+            }
+
             const targetSection = document.querySelector(targetId);
 
             if (targetSection) {
